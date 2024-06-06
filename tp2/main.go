b98e55a94b2b20acf4e589bd7e8183d9b6795577
Sebas <sebastiancormano9@gmail.com>
2024-06-05 23:41:03 -0300

    prueba en main

--- tp2/main.go
+++ tp2/main.go
@@ -1,1 +1,28 @@
-package main
+package tp2
+
+import (
+	"bufio"
+	"os"
+	LecturaComandos "tp2/tp2/comandos"
+)
+
+func main() {
+
+	lectura := bufio.NewScanner(os.Stdin)
+
+	comandoValido := true
+
+	for comandoValido && lectura.Scan() {
+
+		comando := LecturaComandos.CargarComando(lectura)
+
+		comandoValido, informeError := comando.EsComandoValido()
+
+		if comandoValido {
+			comando.EjecutarComando()
+		} else {
+			print(informeError)
+		}
+	}
+
+}
